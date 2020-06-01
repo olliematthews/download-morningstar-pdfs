941c12e2acadda578ccc2fd248a3d5dc1d39e2a4
olliematthews <ojm1@btinternet.com>
2020-06-01 18:49:01 +0100

    class interface

--- download_pdf.py
+++ download_pdf.py
@@ -6,157 +6,49 @@ from pathlib import Path
 import os
 from selenium import webdriver
 from time import sleep
-
-def get_ISIN(fund_id, browser):
-    '''
-    Get the ISIN for the fund.
-
-    Parameters
-    ----------
-    fund_id : str
-        The id for the relevant fund, found after "id=" in the url.
-    browser : webdriver
-
-    Returns
-    -------
-    ISIN : int
-
-    '''
-    url = 'https://www.morningstar.be/be/funds/snapshot/snapshot.aspx?id=' + url_id
-    browser.get(url)
-    if browser.title == 'Morningstar message':
-        browser.get(url)
-    
-    # Click go to website
-    redirect = browser.find_element_by_id('GoDirectToMS')
-    redirect.click()
-    
-    sleep(1)
-    div = browser.find_element_by_id('overviewQuickstatsDiv')
-    table = div.find_element_by_tag_name('table')
-    rows = table.find_elements_by_tag_name('tr')
-    for row in rows:
-        cols = row.find_elements_by_tag_name('td')
-        for col in cols:
-            if col.text == 'ISIN':
-                row_with_ISIN = row
-                
-    ISIN = row_with_ISIN.find_element_by_class_name('text').text
-    return ISIN
-    
-    
-    
-    
-def find_download_pdf(fund_id):
+from web_scraper import WebScraper
+  
+     
+def find_download_pdf(fund_ids):
     '''
     Locate and download the most recent report for a fund.
 
     Parameters
     ----------
-    fund_id : str, int
-        The id for the relevant fund, found after "id=" in the url.
+    fund_id : list
+        The ids for the relevant funds.
 
     Returns
     -------
     ISIN : int
 
     '''
-    try:    
-        fund_id = str(fund_id)
-    except:
-        raise Exception('fund_id should be a string or integer')
+    
+    scraper = WebScraper('C:/Users/Ollie/Downloads/chromedriver_win32/chromedriver')
+    for fund_id in fund_ids:
+        try:    
+            fund_id = str(fund_id)
+        except:
+            raise Exception('fund_id should be a string or integer')
         
-    # We first download the file to the current working directory, then move it
-    download_dir = str(os.path.realpath(os.getcwd()))
+    ISINs = {}
+    for fund_id in fund_ids:
+        ISIN = scraper.get_ISIN(fund_id)
+        
+        
+        # Make sure there is a directory to save to
+        if not os.path.exists('./pdf_downloads'):
+            os.mkdir('./pdf_downloads')
+        
+        scraper.download_pdf(fund_id, './pdf_downloads/' + fund_id + '.pdf')
+        ISINs.update({fund_id : ISIN})
     
-    # Update browser options. This ensures that the pdf is downloaded instead of being viewed in chrome
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs',  {
-        "download.default_directory": download_dir,
-        # "download.prompt_for_download": False,
-        # "download.directory_upgrade": True,
-        # "plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
-        "plugins.always_open_pdf_externally" : True
-        # "download.extensions_to_open": "application/pdf"
-        }
-    )
-    
-    '''
-    # TODO - go headless.
-    
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    '''
-    
-    # Initialise a webdriver with the options set
-    driver_path = Path('C:/Users/Ollie/Downloads/chromedriver_win32/chromedriver')
-    browser = webdriver.Chrome(str(driver_path), options = options)
-    print('Getting ISIN')
-    ISIN = get_ISIN(fund_id, browser)
-    print('ISIN is ' + str(ISIN))
-    print('Navigating to the PDF')
-    url = 'https://www.morningstar.be/be/funds/snapshot/snapshot.aspx?id=' + url_id + '&tab=12'
-
-    browser.get(url)
-    
-    
-    sleep(2)
-    # Find the link to the desired document
-    tables = browser.find_elements_by_tag_name('table')
-    rows = tables[1].find_elements_by_tag_name('tr')
-    
-    # We go through the rows of the table until we find "Verslagen" the next row then contains the most recent document
-    flag = False
-    for row in rows:
-        if flag:
-            row_with_doc = row
-            break
-        cols = row.find_elements_by_tag_name('td')
-        for col in cols:
-            if col.text == 'Verslagen':
-                flag = True
-    # Take the link from that row
-    document_link = row_with_doc.find_element_by_tag_name('a').get_attribute('href')
-    browser.get(document_link)
-
-    # Now we just find the link to the pdf on the document page, and download    
-    sleep(2)
-    pdf_url = browser.find_element_by_id('documentFrame').get_attribute("src")
-    
-    browser.get(pdf_url)
-    
-    print('Downloading...')
-    
-    # Make sure there is a directory to save to
-    if not os.path.exists('./pdf_downloads'):
-        os.mkdir('./pdf_downloads')
-    
-    
-    # Wait until download is finished, then move the file
-    while not 'download.pdf' in os.listdir():
-        pass
-    
-    
-    save_path = Path('./pdf_downloads')
-    save_path /= (url_id + '.pdf')
-    
-    print('File downloaded. Saving it to ' + str(save_path))
-    
-    # If the file is already there, just delete the downloaded file instead
-    try:
-        os.rename('download.pdf', save_path)
-    except FileExistsError:
-        print('File already exists - download aborted.')
-        os.remove('download.pdf')
-    
-    return ISIN
+    return ISINs
 
 
 if __name__ == '__main__':
-    url_id = 'F00000YJ90'
-    find_download_pdf(url_id)
-    url_id = 'F000011JP3'
-    find_download_pdf(url_id)
+    fund_ids = ['F00000YJ90', 'F000011JP3']
+    ISINs = find_download_pdf(fund_ids)
 
 
 
--- web_scraper.py
+++ web_scraper.py
@@ -11,4 +11,177 @@ from time import sleep
 
 
 class WebScraper:
-    def __init__()
+    def __init__(self, driver_path):
+        '''
+        Driver path is the path to the driver for the chromedriver binaries e.g. C:/.../chromedriver
+        '''
+        self.driver_path = driver_path
+
+        # We first download the file to the current working directory, then move it
+        download_dir = str(os.path.realpath(os.getcwd()))
+        
+        # Update browser options. This ensures that the pdf is downloaded instead of being viewed in chrome
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs',  {
+            "download.default_directory": download_dir,
+            "plugins.always_open_pdf_externally" : True
+            }
+        )
+        
+        '''
+        # TODO - go headless.
+        
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        '''
+        
+        # Initialise a webdriver with the options set
+        self.browser = webdriver.Chrome(str(driver_path), options = options)
+
+        
+    def _log_in(self):
+        '''
+        Gets called if the browser finds itself at a screen where it needs to click on a link to go on to the website (usually the first get request). 
+        '''
+        # Click go to website
+        sleep(2)
+        redirect = self.browser.find_element_by_id('GoDirectToMS')
+        redirect.click()
+        
+        # Wait a second to load the next page
+        sleep(1)
+
+    def _get(self, url):
+        '''
+        Runs a get request for a url, with some checks to ensure it works properly.
+        '''
+        self.browser.get(url)
+        
+        # If a 404 error occurs, try waiting and reloading the page, a maximum of 3 times
+        wait_time = 0
+        n_attempts = 1
+        while self.browser.title == 'Morningstar message':
+            sleep(wait_time)
+            self.browser.get(url)
+            wait_time += 1
+            if n_attempts >= 3:
+                raise  Exception('404 error')
+                
+        # If you are met by an intro page, call _log_in
+        if self.browser.current_url.startswith('https://www.morningstar.be/IntroPage'):
+            self._log_in()
+                
+    def get_ISIN(self, fund_id):
+        '''
+        Get the ISIN for the fund.
+    
+        Parameters
+        ----------
+        fund_id : str
+            The id for the relevant fund, found after "id=" in the url.
+        
+        Returns
+        -------
+        ISIN : int
+        '''
+        print('Getting ISIN')
+
+        url = 'https://www.morningstar.be/be/funds/snapshot/snapshot.aspx?id=' + fund_id
+        self._get(url)
+        
+        # Find the table with the ISIN
+        div = self.browser.find_element_by_id('overviewQuickstatsDiv')
+        table = div.find_element_by_tag_name('table')
+        
+        # Find the row with the ISIN
+        rows = table.find_elements_by_tag_name('tr')
+        for row in rows:
+            cols = row.find_elements_by_tag_name('td')
+            for col in cols:
+                if col.text == 'ISIN':
+                    row_with_ISIN = row
+        # Return the ISIN
+        ISIN = row_with_ISIN.find_element_by_class_name('text').text
+        print('ISIN is ' + str(ISIN))
+
+        return ISIN
+
+    def download_pdf(self, fund_id, save_path = None):
+        '''
+        Download the latest report PDF.
+
+        Parameters
+        ----------
+        fund_id : int
+        save_path : str, optional
+            If not specified, the file will be left saved as 'download.pdf'. The default is None.
+
+        '''
+        print('Navigating to the PDF')
+        url = 'https://www.morningstar.be/be/funds/snapshot/snapshot.aspx?id=' + fund_id + '&tab=12'
+    
+        self._get(url)
+        
+        # Find the link to the desired document
+        tables = self.browser.find_elements_by_tag_name('table')
+        rows = tables[1].find_elements_by_tag_name('tr')
+        
+        # We go through the rows of the table until we find "Verslagen" the next row then contains the most recent document
+        flag = False
+        for row in rows:
+            if flag:
+                row_with_doc = row
+                break
+            cols = row.find_elements_by_tag_name('td')
+            for col in cols:
+                if col.text == 'Verslagen':
+                    flag = True
+        # Take the link from that row
+        document_link = row_with_doc.find_element_by_tag_name('a').get_attribute('href')
+        self._get(document_link)
+    
+        # Now we just find the link to the pdf on the document page, and download. We might need to try this a few times while the page loads
+        wait_time = 1
+        n_attempts = 1
+        while n_attempts < 3:
+            try:
+                pdf_url = self.browser.find_element_by_id('documentFrame').get_attribute("src")
+                break
+            except:
+                sleep(wait_time)
+                
+        self.download_name = 'download.pdf'
+        if self.download_name in os.listdir():
+            i = 1
+            while self.download_name in os.listdir():
+                self.download_name = 'download (' + str(i) + ').pdf'
+                i += 1
+        
+        self.browser.get(pdf_url)
+        
+        print('Downloading...')
+        
+        # Wait for download to come through before returning
+        while not self.download_name in os.listdir():
+            sleep(0.5)
+        print('Downloaded')
+        
+        if not save_path is None:
+            self.rename_download(save_path)
+        
+    def rename_download(self, save_path):
+        '''
+        Rename the downloaded file. If it is already there, then just delete the download.
+        
+        Parameters
+        ----------
+        save_path : str
+        '''
+        print('Moving the file to ' + save_path)
+        # If the file is already there, just delete the downloaded file instead
+        try:
+            os.rename('download.pdf', save_path)
+        except FileExistsError:
+            print('File already exists - removing downloaded file.')
+            os.remove('download.pdf')
+            
